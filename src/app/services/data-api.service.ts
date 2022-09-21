3472cd61a5a433881659da01dbd2acabda7acfcb
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-09-21 15:18:20 -0400

    pulish

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -26,7 +26,7 @@ export class DataApiService {
 	});
 		  getCardByUserId(userId: string){
 			 	const url_api = `https://db.buckapi.com:3069/api/cards?filter[where][userId]=${userId}`;
-			 	this.cards = this.http.get<card[]>(url_api);
+			 	this.cards = this.http.get(url_api);
 			//	this.butler.idBuckapicard=this.card[0].id;
 			//	this.butler.idApp=this.card[0].idApp;
 			 	return ( this.http.get(url_api));
