fbee2594f52f74b6d2e6045ea14483476269436e
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-09-21 13:03:45 -0400

    admintrue card service

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -4,6 +4,7 @@ export const CARDS: Card[] = [
   {
     email:'andreazambrano10@gmail.com',
     userType: 'admin',
+    admin:true,
     userId:'68177ed3-70e5-4bef-b1d3-80530ccfbd6a',
     images:[
       'assets/assetsdash/images/avatar-7.jpg',
