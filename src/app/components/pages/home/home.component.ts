535ce6c3d01a88352234779fe38c458bda77e1cf
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-09-21 16:59:43 -0400

    card$

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -167,8 +167,7 @@ getDirections(place:Feature){
     .getCardByUserId(this._butler.userId)
     .subscribe((
       card$:BuckapicardInterface) => (this.card$=card$),
-      console.log(this.card$)
-    
+      console.log(this.card$)    
     );
   }
   ngAfterViewInit(): void {
